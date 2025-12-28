c7a96ca78e127ca6c9cc310c87733bddda863032
Jason <jasonhughes634@gmail.com>
2025-12-28 14:46:42 +0100

    Fix Run.py comments for running code locally

--- run.py
+++ run.py
@@ -9,23 +9,23 @@ app = create_app()
 if __name__ == "__main__":
     app.run(debug=True)
 
-"""
+
 # how to use app, run these lines of code in your terminal or git bash
 
 # Create virtual environment (recommended)
-python -m venv venv
+# python -m venv venv
 
 # Activate it:
 # Windows:
-venv\Scripts\activate
+#  venv\Scripts\ activate
 
 # Mac/Linux:
-source venv/bin/activate
+# source venv/bin/activate
 
 # You should see (venv) in terminal prompt
 
 # Make sure venv is activated, then:
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 # Create a .env file in project root
 # (You should use the .env.example template to create yours)
@@ -33,13 +33,12 @@ pip install -r requirements.txt
 
 
 # Initialize database
-flask init-db  
+# flask init-db
 
 # Method 1: Using run.py (if you have it)
-python run.py
+# python run.py
 
 # Method 2: Using Flask CLI
-flask run
+# flask run
 
 # The app will be at: http://localhost:5000
-"""
