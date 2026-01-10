e635bf78270679bd6aa77f290cc43f3aa10dcf9e
Jason <jasonhughes634@gmail.com>
2026-01-10 17:55:58 +0100

    Add gunicorn config, and a Procfile

---
+++ myapp/gunicorn_production.py
@@ -0,0 +1,25 @@
+# gunicorn_config.py
+import multiprocessing
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Server socket
+bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# Logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+
+# Process naming
+proc_name = 'my_flask_api'
