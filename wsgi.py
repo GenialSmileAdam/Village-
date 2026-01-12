f6748bc857c770d13c8652f45543475391279627
VEOBTS <demejiayomide@gmail.com>
2026-01-12 04:31:55 +0100

    integration of error logging

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-"""Production WSGI entry point for Gunicorn/uWGSI"""
-
-from myapp import create_app
-
-app = create_app("production")
-
-if __name__ == '__main__':
-    # This allows running: python wsgi.py (for testing)
-    app.run()
