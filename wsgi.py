ac4179acc41943b1a451bfcb923e1fb5d4421297
Jason <jasonhughes634@gmail.com>
2026-02-17 15:54:42 +0100

    Deployment fixes

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,12 @@
 """Production WSGI entry point for Gunicorn/uWGSI"""
 
 from myapp import create_app
+from myapp import db
 
 app = create_app("production")
+with app.app_context():
+    db.create_all()
+    app.logger.info("Database tables checked/created successfully")
 
 if __name__ == '__main__':
     # This allows running: python wsgi.py (for testing)
